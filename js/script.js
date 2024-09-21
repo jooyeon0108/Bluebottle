4ff0e17c75c3937d967b11ec98a71b8c0e435775
jooyeon0108 <hun550@naver.com>
2024-09-21 20:16:34 +0900

    2024-09-21

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
 $(document).ready(function(){
 
     // 슬라이드
-    function slide(){
-        $('.slide ul').animate({left: "-100%"},1300,function(){
-            $('.slide ul').append($('.slide ul li').first());
-            $('.slide ul').css({left: 0});
-        })
-    }
-    setInterval(slide, 3800)
+    // function slide(){
+    //     $('.slide ul').animate({left: "-100%"},1300,function(){
+    //         $('.slide ul').append($('.slide ul li').first());
+    //         $('.slide ul').css({left: 0});
+    //     })
+    // }
+    // setInterval(slide, 3800)
+
+    // 슬라이드
+    let swiper = new Swiper('.swiper', {
+        // Optional parameters
+        spaceBetween: 30,
+
+        effect: 'fade',
+        observer: true,
+        observeParents: true,
+        loop: true,
+        autoplay : {
+            delay: 3000,
+            disableOnInteraction: false // 사용자 인터랙션 후에도 autoplay 재시작
+        },
+        speed: 1000,
+        
+        // If we need pagination
+        pagination: {   // 슬라이드 넘버
+          el: '.swiper-pagination',
+          clickable: true
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      
+    });
+    
 
 
     // 반응형 
@@ -17,7 +47,7 @@ $(document).ready(function(){
     });
 
     function checkWindowSize(){
-        var windowWidth = $(window).width();
+        let windowWidth = $(window).width();
 
         if (windowWidth >= 1200) {
             // pc 
@@ -35,6 +65,10 @@ $(document).ready(function(){
                 }).on('mouseout', function(){
                     $('.sub, .menubg').stop().slideUp();
                 })   
+
+            // Swiper autoplay가 PC 전환 시 멈추지 않도록 재시작
+            // swiper.autoplay.start(); 
+
         } else {
             // mobile + tablet
             // pc에서 설정한 마우스 이벤트 제거
@@ -44,24 +78,24 @@ $(document).ready(function(){
             $('nav').css({left: "-100%"});
 
             // 모바일 메뉴 열기/닫기 애니메이션
-            $('.btn-menu').off('click').on('click', function(){
-                $('nav').animate({left: 0});
+            $('.btn-menu').click(function(){
+                $('nav').css({left: 0});
             });
-            $('.btn-cls').off('click').on('click', function(){
-                $('nav').animate({left: "-100%"});
+            $('.btn-cls').click(function(){
+                $('nav').css({left: "-100%"});
             });
              // 포커스
             $('.btn-menu').focusin(function(){
-                $('nav').animate({left: 0});
+                $('nav').css({left: 0});
             })
             $('nav>ul>li:last-child li:last-child').focusout(function(){
-                $('nav').animate({left: "-100%"}); // '.btn-cls'에 포커스 마지막으로 오고, 한번더 어떤 동작하면 nav가 닫히게 하고 싶은데 못했다.
+                $('nav').css({left: "-100%"}); // '.btn-cls'에 포커스 마지막으로 오고, 한번더 어떤 동작하면 nav가 닫히게 하고 싶은데 못했다.
             })
 
 
             $('nav>ul>li').off('click').on('click', function(){
                 // 클릭한 메뉴의 서브 메뉴만 슬라이드 토글(펼치거나 숨김) 
-                $(this).find('.sub').stop().slideToggle();
+                $(this).find('.sub').stop().slideDown();
                 // 다른 서브 메뉴를 모드 슬라이드 업(숨김)
                 $(this).siblings().find('.sub').stop().slideUp();                
             })
@@ -74,6 +108,8 @@ $(document).ready(function(){
                 $(this).find('.sub').stop().slideUp();
             })
 
+            // 모바일/태블릿 전환 시에도 Swiper autoplay가 멈추지 않도록 보장
+            // swiper.autoplay.start();
         }
     }
 
