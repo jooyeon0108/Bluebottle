632f48fdb6cc39207e9e0a6dfdcde7a3ca7903a3
jooyeon0108 <hun550@naver.com>
2024-10-14 14:47:09 +0900

    2024-10-14

--- js/script.js
+++ js/script.js
@@ -39,6 +39,16 @@ $(document).ready(function () {
             // 모바일에 남아있는 클래스 초기화
             $('nav>ul>li .sub').removeClass('show').hide();
 
+            // pc 메뉴 키보드이벤트
+            // $('nav>ul>li').off('focusin').on('focusin', function(){
+            //     $('.sub, .menubg').stop().slideDown();
+            // });
+            // $('nav>ul>li:last-child li:last-child').off('focusout').on('focusout', function(){
+            //     $('.sub, .menubg').stop().slideUp();
+            // });
+
+           
+
         } else {
             // mobile + table
             swiper.autoplay.start();
@@ -62,6 +72,20 @@ $(document).ready(function () {
                 $(this).children('.sub').stop().slideToggle().toggleClass('show');
                 $(this).siblings().children('.sub').stop().slideUp().removeClass('show');
             });   
+
+            // 모바일 메뉴 키보드이벤트 
+            //  $('.btn-menu').off('focusin').on('focusin', function(){
+            //     $('nav').css({left: 0});
+            // });
+            // $('nav>ul>li').off('focusin').on('focusin', function(){
+            //     $(this).children('.sub').stop().slideDown(); 
+            // });
+            // $('nav>ul>li').off('focusout').on('focusout', function(){
+            //     $(this).children('.sub').stop().slideUp();
+            // });
+            // $('.nav-util>li:last-child').off('focusout').on('focusout', function(){
+            //     $('nav').css({left: '-100%'});
+            // })
         }
     }
 
